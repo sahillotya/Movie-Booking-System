5601f5d64bc880f35c7961013c4ff741bd72caff
sahillotya <sahillotya021@gmail.com>
2020-11-10 19:08:35 +0530

    php database connection

--- contact.php
+++ contact.php
@@ -12,7 +12,7 @@ if(isset($_POST['SubmitButton'])){ //check if form was submitted
     $current_id = mysqli_query($conn, $sqlreg) or die("<b>Error:</b> Problem while submitting<br/>" . mysqli_error($conn));
     $message = "Success! You entered: ";
     if($current_id){
-        header("Location:index.php");
+        header("Location:index.html");
     }
 } 
 
